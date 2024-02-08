6eaaf6377120b002b3a8c0903f34b93b895d2365
Roma Vintonyak <19roman23@gmail.com>
2024-02-08 14:27:43 +0200

    finished nav menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,13 @@
 jQuery(document).ready(function () {
   "use script";
- 
-  var btn = $("._callback");
-  btn.on("click", function(){
-    alert("Its work");
-    prompt();
-  });
+  /*burger button*/
+  var wWidth = $(window).width();
+  var burgerBtn = $(".burger__btn");
+  if (wWidth <= 980) {
+    burgerBtn.on("click", function () {
+      $(this).toggleClass("burger__btn--active");
+    });
+  }
+
+
 });
