5d3ef5799205e7d2ee6f1ae7e6dcf863a45961d1
Roma Vintonyak <19roman23@gmail.com>
2024-07-29 19:28:09 +0300

    create chackout page

--- app/js_src/chart.js
+++ app/js_src/chart.js
@@ -28,16 +28,50 @@ jQuery(document).ready(function () {
    }
    allSumm();
    /*remove product position*/
-   $("._removeBlock").on("click", function(){
-      if(confirm("Бажаєте видалити дану позицію?")){
+   $("._removeBlock").on("click", function () {
+      if (confirm("Бажаєте видалити дану позицію?")) {
          $(this).closest(".chart__block--delete").closest(".chart__block").remove();
          allSumm();
-      }else{
+      } else {
          return false;
       }
    });
+   /*count minus*/
+   $(".chart__minus").on("click", function (event) {
+      event.preventDefault();
+      var counterMinus = $(this).closest(".chart__tovar--count").find("._chartCount");
+      var countMinus = parseInt(counterMinus.val()) - 1;
+      countMinus = countMinus < 1 ? 1 : countMinus;
+      counterMinus.val(countMinus);
+      counterMinus.change();
+      var tovarPriceM = $(this).closest(".chart__tovar--count")
+         .closest(".chart__block--content").find("[chart-price]").text();
+      var tovarCountM = $(this).closest(".chart__tovar--count").find("._chartCount").val().trim();
+      var tovarSumm = tovarPriceM * tovarCountM;
+      var tovarSummM = $(this).closest(".chart__tovar--count")
+         .closest(".chart__block--content").find("[data-chartsumm]");
+      tovarSummM.html(tovarSumm);
+      allSumm();
+      return false;
+   });
+   /*count plus*/
+   $(".chart__plus").on("click", function (event) {
+      event.preventDefault();
+      var counertPlus = $(this).closest(".chart__tovar--count").find("._chartCount");
+      counertPlus.val(parseInt(counertPlus.val()) + 1);
+      counertPlus.change();
+      var tovarPriceP = $(this).closest(".chart__tovar--count")
+         .closest(".chart__block--content").find("[chart-price]").text();
+      var tovarCountP = $(this).closest(".chart__tovar--count").find("._chartCount").val().trim();
+      var tovarSummP = tovarPriceP * tovarCountP;
+      var tovarSummPblock = $(this).closest(".chart__tovar--count")
+         .closest(".chart__block--content").find("[data-chartsumm]");
+      tovarSummPblock.html(tovarSummP);
+      allSumm();
+      return false;
+   });
    /*reload chart page*/
-   $("#reloadPage").on("click", function(){
+   $("#reloadPage").on("click", function () {
       location.reload(true);
    });
 });
